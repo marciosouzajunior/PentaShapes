78a7a58dd07ccfcefd8df859a51376d492837ff1
Márcio de Souza Júnior <marciosouzajunior@gmail.com>
2025-04-17 10:12:43 -0300

    render shapes start

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ var noteToShow = "All";
 var canClick = true;
 
 var notes = {
-	e0: ['F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A'],
-	b:  ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E'],
-	g:  ['G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C'],
-	d:  ['D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G'],
-	a:  ['A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', "A", 'A#', 'B', 'C', 'C#', 'D'],
-	e1: ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+	e_high: ['F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A'],
+	b:  	['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E'],
+	g:  	['G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C'],
+	d:  	['D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G'],
+	a:  	['A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', "A", 'A#', 'B', 'C', 'C#', 'D'],
+	e_low:  ['F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A'],
 }
 
-for (var i = 0; i < notes.e0.length; i++) {
-	$('.mask.low-e ul').append('<li note=' + notes.e1[i] + '>' + notes.e1[i] + '</li>')
-	$('.mask.b ul').append('<li note=' + notes.b[i] + '>' + notes.b[i] + '</li>')
-	$('.mask.d ul').append('<li note=' + notes.d[i] + '>' + notes.d[i] + '</li>')
-	$('.mask.g ul').append('<li note=' + notes.g[i] + '>' + notes.g[i] + '</li>')
-	$('.mask.a ul').append('<li note=' + notes.a[i] + '>' + notes.a[i] + '</li>')
-	$('.mask.high-e ul').append('<li note=' + notes.e0[i] + '>' + notes.e0[i] + '</li>')
+for (var i = 0; i < notes.e_low.length; i++) {
+	$('.mask.high-e ul').append('<li class="inactive" data-index=' + i + ' data-note=' + notes.e_high[i] 	+ ' onclick="renderShapes(\'e_high\',' 	+ i + ')">' + notes.e_high[i] + '</li>')
+	$('.mask.b ul')		.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.b[i] 		+ ' onclick="renderShapes(\'b\',' 		+ i + ')">' + notes.b[i] + '</li>')
+	$('.mask.d ul')		.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.d[i] 		+ ' onclick="renderShapes(\'d\',' 		+ i + ')">' + notes.d[i] + '</li>')
+	$('.mask.g ul')		.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.g[i] 		+ ' onclick="renderShapes(\'g\',' 		+ i + ')">' + notes.g[i] + '</li>')
+	$('.mask.a ul')		.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.a[i] 		+ ' onclick="renderShapes(\'a\',' 		+ i + ')">' + notes.a[i] + '</li>')
+	$('.mask.low-e ul')	.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.e_low[i] 	+ ' onclick="renderShapes(\'e_low\',' 	+ i + ')">' + notes.e_low[i] + '</li>')
 }
-
+/*
 $('.controls a.down').click(function () {
 	if (!canClick) { return false; }
 	canClick = false;
@@ -91,4 +91,34 @@ function changeOpenNotes() {
 
 		$('.open-notes .' + elClass).text(note);
 	});
+}
+*/
+function inactiveAll(){
+	$('.guitar-neck .notes li').removeClass('active');
+	$('.guitar-neck .notes li').addClass('inactive');
+}
+
+function renderShapes(string, index) {
+
+	inactiveAll();
+
+	console.log(string, index);
+
+	// TODO: Add logic to render shapes for other strings
+	if (string == 'e_high' || string == 'b' || string == 'g') {
+		alert('This string is not available. Select the root note in lower strings.');
+		return;
+	}
+	var note = notes[string][index];
+	console.log(note);
+	// activate the selected note
+	$('.mask[data-string="' + string + '"] li[data-note="' + note + '"]').removeClass('inactive');
+	$('.mask[data-string="' + string + '"] li[data-note="' + note + '"]').addClass('active');
+	//console.log('.guitar-neck .notes div[data-string="e_low"] .' + string + ' li[data-note="' + note + '"]');
+	
+	
+
+	
+
+
 }
