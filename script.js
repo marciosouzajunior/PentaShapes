d7b3e819ca2e85d046947d1c495eb48d64873d65
Márcio de Souza Júnior <marciosouzajunior@gmail.com>
2025-04-17 18:14:49 -0300

    more render shapes

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var slideSpeed = 300;
-var noteToShow = "All";
-var canClick = true;
 
 var notes = {
 	e_high: ['F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A'],
@@ -19,80 +16,41 @@ for (var i = 0; i < notes.e_low.length; i++) {
 	$('.mask.a ul')		.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.a[i] 		+ ' onclick="renderShapes(\'a\',' 		+ i + ')">' + notes.a[i] + '</li>')
 	$('.mask.low-e ul')	.append('<li class="inactive" data-index=' + i + ' data-note=' + notes.e_low[i] 	+ ' onclick="renderShapes(\'e_low\',' 	+ i + ')">' + notes.e_low[i] + '</li>')
 }
-/*
-$('.controls a.down').click(function () {
-	if (!canClick) { return false; }
-	canClick = false;
 
-	$('.mask').each(function () {
-		var el = $(this);
-		var nextNote = el.find('li:nth-child(12)').text();
-
-		el.animate({ right: -268 }, slideSpeed);
-		setTimeout(function () {
-			el.find('ul').prepend("<li note=" + nextNote + ">" + nextNote + "</li>");
-			el.find('li:last-child').remove();
-			el.css({ right: -189 });
-		}, slideSpeed + 20)
-	});
-
-	setTimeout(function () {
-		changeOpenNotes();
-		showNotes(noteToShow);
-		canClick = true;
-	}, slideSpeed + 20)
-
-	return false;
-});
-
-$('.controls a.up').click(function () {
-	if (!canClick) { return false; }
-	canClick = false;
-
-	$('.mask').each(function () {
-		var el = $(this);
-		var nextNote = el.find('li:nth-child(2)').text();
-
-		$("<li note=" + nextNote + ">" + nextNote + "</li>").appendTo(el.find('ul'));
-		el.css({ right: -268 });
-		el.find('li:first-child').remove();
-		el.animate({ right: -189 }, slideSpeed);
-
-	});
-
-	changeOpenNotes();
-	showNotes(noteToShow);
-
-	setTimeout(function () {
-		canClick = true;
-	}, slideSpeed + 20)
-	return false;
-});
-
-$('.controls li').click(function () {
-	noteToShow = $(this).text();
-	showNotes(noteToShow);
-});
-
-function showNotes(noteToShow) {
-	if (noteToShow == "All") {
-		$('.guitar-neck .notes li').animate({ opacity: 1 }, 500);
-	} else {
-		$('.guitar-neck .notes li').not('[note="' + noteToShow + '"]').animate({ opacity: 0 }, 500);
-		$('.guitar-neck .notes li[note="' + noteToShow + '"]').animate({ opacity: 1 }, 500);
+var penta_min = {
+	e_low: {
+	  shape_1: [
+		{ string: 'e_high', offset: 0, label: '1' },
+		{ string: 'e_high', offset: 3, label: '♭3' },
+		{ string: 'b', offset: 0, label: '5' },
+		{ string: 'b', offset: 3, label: '♭7' },
+		{ string: 'g', offset: 0, label: '♭3' },
+		{ string: 'g', offset: 2, label: '4' },
+		{ string: 'd', offset: 0, label: '♭7' },
+		{ string: 'd', offset: 2, label: '1' },
+		{ string: 'a', offset: 0, label: '4' },
+		{ string: 'a', offset: 2, label: '5' },
+		{ string: 'e_low', offset: 0, label: '1' },
+		{ string: 'e_low', offset: 3, label: '♭3' }
+	  ],
+	  shape_5: [
+		{ string: 'e_high', offset: 0, label: '1' },
+		{ string: 'e_high', offset: -2, label: '♭7' },
+		{ string: 'b', offset: 0, label: '5' },
+		{ string: 'b', offset: -2, label: '4' },
+		{ string: 'g', offset: 0, label: '♭3' },
+		{ string: 'g', offset: -3, label: '1' },
+		{ string: 'd', offset: 0, label: '♭7' },
+		{ string: 'd', offset: -3, label: '5' },
+		{ string: 'a', offset: 0, label: '4' },
+		{ string: 'a', offset: -2, label: '♭3' },
+		{ string: 'e_low', offset: 0, label: '1' },
+		{ string: 'e_low', offset: -2, label: '♭7' }
+	  ]
 	}
-}
+  };
 
-function changeOpenNotes() {
-	$('.notes .mask').each(function () {
-		var el = $(this);
-		var elClass = el.attr('class').split(' ')[1];
-		var note = el.find('li:last-child').text();
 
-		$('.open-notes .' + elClass).text(note);
-	});
-}
-*/
 function inactiveAll(){
 	$('.guitar-neck .notes li').removeClass('active');
 	$('.guitar-neck .notes li').addClass('inactive');
@@ -102,23 +60,66 @@ function renderShapes(string, index) {
 
 	inactiveAll();
 
-	console.log(string, index);
-
 	// TODO: Add logic to render shapes for other strings
 	if (string == 'e_high' || string == 'b' || string == 'g') {
 		alert('This string is not available. Select the root note in lower strings.');
 		return;
 	}
-	var note = notes[string][index];
-	console.log(note);
+
+	// Display the root note
+	var rootNote = notes[string][index];
+	$('#root-note').text(rootNote);
+
+	// Get selected scale
+	var selectedScale = $('#scale-select').val();
+	console.log(selectedScale);
+
+	// If is penta_min
+	if (selectedScale == 'penta_min') {
+
+		// Get available shapes
+		var shapes = penta_min[string];
+		//console.log(shapes);
+
+		// Iterate the shapes and notes
+		for (var shape in shapes) {
+
+			var shapeData = shapes[shape];
+
+			for (var noteData of shapeData) {
+
+				var stringName = noteData.string;
+				var offset = noteData.offset;
+				var label = noteData.label;
+
+				$('.mask[data-string="' + stringName + '"] li[data-index="' + (offset + index) + '"]').removeClass('inactive');
+				$('.mask[data-string="' + stringName + '"] li[data-index="' + (offset + index) + '"]').addClass('active');
+				
+			}
+
+		}
+
+
+	}
+
+
+
 	// activate the selected note
-	$('.mask[data-string="' + string + '"] li[data-note="' + note + '"]').removeClass('inactive');
-	$('.mask[data-string="' + string + '"] li[data-note="' + note + '"]').addClass('active');
+	//$('.mask[data-string="' + string + '"] li[data-note="' + rootNote + '"]').removeClass('inactive');
+	//$('.mask[data-string="' + string + '"] li[data-note="' + rootNote + '"]').addClass('active');
 	//console.log('.guitar-neck .notes div[data-string="e_low"] .' + string + ' li[data-note="' + note + '"]');
 	
+
+
 	
 
 	
 
 
+}
+
+function getShapesByString(data, stringName) {
+	if (!data[stringName]) return [];
+
+	return Object.keys(data[stringName]);
 }
