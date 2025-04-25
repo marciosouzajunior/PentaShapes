f4b44be0586077a1aa754762a0250aff99e80476
Márcio de Souza Júnior <marciosouzajunior@gmail.com>
2025-04-25 17:27:13 -0300

    blues

--- scales.js
+++ scales.js
@@ -1,4 +1,47 @@
 const scales = (() => {
+    
+    const chromatic = {
+        e_low: {
+            shape_1: [
+                { string: 'e_high', offset: 5, label: 'p4' },
+                { string: 'e_high', offset: 4, label: '∆3' },
+                { string: 'e_high', offset: 3, label: '♭3' },
+                { string: 'e_high', offset: 2, label: '∆2' },
+                { string: 'e_high', offset: 1, label: '♭2' },
+
+                { string: 'b', offset: 5, label: '8' },
+                { string: 'b', offset: 4, label: '∆7' },
+                { string: 'b', offset: 3, label: '♭7' },                
+                { string: 'b', offset: 2, label: '∆6' },
+                { string: 'b', offset: 1, label: '♭6' },
+
+                { string: 'g', offset: 4, label: 'p5' },
+                { string: 'g', offset: 3, label: '♭5' },
+                { string: 'g', offset: 2, label: 'p4' },
+                { string: 'g', offset: 1, label: '∆3' },
+                { string: 'g', offset: 0, label: '♭3' }
+                ,
+                { string: 'd', offset: 4, label: '∆2' },
+                { string: 'd', offset: 3, label: '♭2' },
+                { string: 'd', offset: 2, label: '8' },
+                { string: 'd', offset: 1, label: '∆7' },
+                { string: 'd', offset: 0, label: '♭7' },
+                
+                { string: 'a', offset: 4, label: '∆6' },
+                { string: 'a', offset: 3, label: '♭6' },
+                { string: 'a', offset: 2, label: 'p5' },
+                { string: 'a', offset: 1, label: '♭5' },
+                { string: 'a', offset: 0, label: 'p4' },
+
+                { string: 'e_low', offset: 4, label: '∆3' },
+                { string: 'e_low', offset: 3, label: '♭3' },
+                { string: 'e_low', offset: 2, label: '∆2' },
+                { string: 'e_low', offset: 1, label: '♭2' },
+                { string: 'e_low', offset: 0, label: '1' }            
+            ]
+        }
+
+    };
 
     const penta_min = {
         e_low: {
@@ -89,6 +132,43 @@ const scales = (() => {
                 { string: 'a', offset: 0, label: '5' },
                 { string: 'e_low', offset: 3, label: '4' },
                 { string: 'e_low', offset: 1, label: '♭3' }
+            ]
+        }
+    };
+
+    const blues = {
+        e_low: {
+            shape_1: [
+                { string: 'e_high', offset: 3, label: '♭3' },
+                { string: 'e_high', offset: 0, label: '1' },
+                { string: 'b', offset: 3, label: '♭7' },
+                { string: 'b', offset: 0, label: '5' },
+                { string: 'g', offset: 3, label: '♭5' },
+                { string: 'g', offset: 2, label: '4' },
+                { string: 'g', offset: 0, label: '♭3' },
+                { string: 'd', offset: 2, label: '1' },
+                { string: 'd', offset: 0, label: '♭7' },
+                { string: 'a', offset: 2, label: '5' },
+                { string: 'a', offset: 1, label: '♭5' },
+                { string: 'a', offset: 0, label: '4' },
+                { string: 'e_low', offset: 3, label: '♭3' },
+                { string: 'e_low', offset: 0, label: '1' }
+            ],
+            shape_2: [
+                { string: 'e_high', offset: 5, label: '4' },
+                { string: 'e_high', offset: 3, label: '♭3' },
+                { string: 'b', offset: 5, label: '1' },
+                { string: 'b', offset: 3, label: '♭7' },
+                { string: 'g', offset: 4, label: '5' },
+                { string: 'g', offset: 3, label: '♭5' }
+            ],
+            shape_3: [
+                { string: 'e_high', offset: 7, label: '5' },
+                { string: 'e_high', offset: 5, label: '4' },
+                { string: 'b', offset: 8, label: '♭3' },
+                { string: 'b', offset: 7, label: '2' },
+                { string: 'b', offset: 5, label: '1' },
+                { string: 'g', offset: 6, label: '6' }
             ]
         }
     };
@@ -186,53 +266,12 @@ const scales = (() => {
         }
     };
 
-    const chromatic = {
-        e_low: {
-            shape_1: [
-                { string: 'e_high', offset: 5, label: 'p4' },
-                { string: 'e_high', offset: 4, label: '∆3' },
-                { string: 'e_high', offset: 3, label: '♭3' },
-                { string: 'e_high', offset: 2, label: '∆2' },
-                { string: 'e_high', offset: 1, label: '♭2' },
-
-                { string: 'b', offset: 5, label: '8' },
-                { string: 'b', offset: 4, label: '∆7' },
-                { string: 'b', offset: 3, label: '♭7' },                
-                { string: 'b', offset: 2, label: '∆6' },
-                { string: 'b', offset: 1, label: '♭6' },
-
-                { string: 'g', offset: 4, label: 'p5' },
-                { string: 'g', offset: 3, label: '♭5' },
-                { string: 'g', offset: 2, label: 'p4' },
-                { string: 'g', offset: 1, label: '∆3' },
-                { string: 'g', offset: 0, label: '♭3' }
-                ,
-                { string: 'd', offset: 4, label: '∆2' },
-                { string: 'd', offset: 3, label: '♭2' },
-                { string: 'd', offset: 2, label: '8' },
-                { string: 'd', offset: 1, label: '∆7' },
-                { string: 'd', offset: 0, label: '♭7' },
-                
-                { string: 'a', offset: 4, label: '∆6' },
-                { string: 'a', offset: 3, label: '♭6' },
-                { string: 'a', offset: 2, label: 'p5' },
-                { string: 'a', offset: 1, label: '♭5' },
-                { string: 'a', offset: 0, label: 'p4' },
-
-                { string: 'e_low', offset: 4, label: '∆3' },
-                { string: 'e_low', offset: 3, label: '♭3' },
-                { string: 'e_low', offset: 2, label: '∆2' },
-                { string: 'e_low', offset: 1, label: '♭2' },
-                { string: 'e_low', offset: 0, label: '1' }            
-            ]
-        }
-
-    };
 
     return { 
         chromatic,
         penta_min,
-        penta_maj
+        penta_maj,
+        blues
     };
 
 })();
